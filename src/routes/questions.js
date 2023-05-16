af8043fcafb7c441e88cbcc48098ffd24d4fe888
jimmycooks6748 miedema <graysenmiedema@gmail.com>
2023-05-16 15:06:03 +0000

    questions fr

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -7,11 +7,11 @@
  *  */
     export const questions = [
         {
-            question: "Is this the first question?",
+            question: "Ancient Romans Boiled Vinegar And What To Make An Energy Drink??",
             answers: [
-                'Yes',
-                'No',
-                'Maybe'
+                'Goat Poop',
+                'Cherries',
+                'Lemon'
             ],
             correct: 0,
             time: 20
@@ -23,7 +23,7 @@
                 'Jim',
                 'Lazlo'
             ],
-            correct: 1,
+            correct: 2,
             time: 20
         },
         {
@@ -39,13 +39,174 @@
     
         },
         {
-            question: "Who was the first U.S cowboy",
+            question: "Who were the first U.S cowboys",
             answers: [
                 'Vaquors',
-                'Minons',
+                'Minions',
                 'Mexicans'
             ],
             correct: 0,
             time: 20
-        }
+        },
+    {
+        question: "How many super bowls have there been?",
+        answers: [
+            '50',
+            '55',
+            '45',
+            '68',
+        ],
+        correct: 1,
+        time: 20
+    },
+
+    {
+        question: "What Is Scotland's National Animal??",
+        answers: [
+            'Horse',
+            'Cat',
+            'Unicorn',
+            'Rock',
+        ],
+        correct: 2,
+        time: 20
+    },
+ {
+        question: "A group of ravens is called?",
+        answers: [
+            'Flock',
+            'Ching',
+            'Unkindness',
+        ],
+        correct: 2,
+        time: 20
+    },
+
+    {
+        question: "Who was the 13th president?",
+        answers: [
+            'John Jay',
+            'Millard Filmore',
+            'Andrew Johnson'
+        ],
+        correct: 1,
+        time: 20
+    },
+ {
+        question: "Why Did Pirates Wear Earrings?",
+        answers: [
+            'Stomach Aches',
+            'Eyesight',
+            'For kicks'
+        ],
+        correct: 1,
+        time: 20
+    },
+    {
+        question: " In Ancient Greece, Throwing An Apple At Someone Was A Declaration Of What??",
+        answers: [
+            'Love',
+            'Despair',
+            'Hate'
+        ],
+        correct: 0,
+        time: 20
+    },
+
+    {
+        question: "In North Carolina, It's Illegal To Do What??",
+        answers: [
+            'Steal',
+            'Kill',
+            'Sing out of tune'
+        ],
+        correct: 2,
+        time: 20
+    },
+
+    {
+        question: "When Do Babies Get Their Fingerprints??",
+        answers: [
+            '1 yr',
+            '21 weeks',
+            '6 months',
+            '3 days'        ],
+        correct: 1,
+        time: 20
+    },
+
+    {
+        question: "What Bird Has Eyes That Are Bigger Than Its Brain?",
+        answers: [
+            'Robin',
+            'Ostrich',
+            'Penguin'
+        ],
+        correct: 1,
+        time: 20
+    },
+
+    {
+        question: "What Reptile Can't Stick Its Tongue Out???",
+        answers: [
+            'Lizards',
+            'Crocs',
+            'Snakes'
+        ],
+        correct: 1,
+        time: 20
+    },
+
+ {
+        question: "In The U.S.A., Every 12 Seconds Someone Does What In A Holiday Inn??",
+        answers: [
+            'Steal a towel',
+            'Check out',
+            'Swim'
+        ],
+        correct: 0,
+        time: 20
+    },
+ 
+    {
+        question: " In Florida, It's Illegal For Unmarried Women To Do What On Sundays??",
+        answers: [
+            'Skydive',
+            'Do Laundry',
+            'Go on dates',
+            'Clean',
+        ],
+        correct: 0,
+        time: 20
+    },
+  
+    {
+        question: "What do americans do 22 times a day?",
+        answers: [
+            'Yawn',
+            'Sneeze',
+            'Open the fridge',
+            'Cry',
+        ],
+        correct: 2,
+        time: 20
+    },
     ]
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
